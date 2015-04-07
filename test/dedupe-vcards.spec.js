37afe26dc99dc43a824cdea330626b41e67181b4
Nils Norman Haukås <nils@thunki.com>
2015-04-07 19:56:39 +0200

    updated tests

--- test/dedupe-vcards.spec.js
+++ test/dedupe-vcards.spec.js
@@ -1,8 +1,9 @@
-var assert = require("assert")
-var dedupeVcards = require('../dedupe-vcards.js')
+var assert = require("assert");
+var dedupeVcards = require('../dedupe-vcards.js');
+var _ = require('lodash');
 
 describe('vcard program', function(){
-  it('processVcardData() should be able to create vcardArrays', function(){
+  it('createVcardArray() should be able to create vcardArrays', function(){
     var data = "BEGIN:VCARD\n" +
     "N:Test\n" +
     "END:VCARD\n" +
@@ -13,5 +14,34 @@ describe('vcard program', function(){
     var desiredResult = [["BEGIN:VCARD", "N:Test", "END:VCARD"],
     ["BEGIN:VCARD", "FN:TestName", "END:VCARD"]];
     assert.deepEqual(actualResult, desiredResult);
+  });
+
+  it('processVcardFiles() should be able to concatenate and remove duplicates', function (done) {
+    var desiredResult = [["BEGIN:VCARD",
+    "VERSION:3.0",
+    "FN:TestName Will be reduced to one",
+    "N:",
+    "TEL;TYPE=CELL:",
+    "END:VCARD"],
+    ["BEGIN:VCARD",
+    "VERSION:3.0",
+    "FN:Not a duplicate",
+    "N:",
+    "TEL;TYPE=CELL:",
+    "END:VCAR"]];
+    dedupeVcards.processVcardFiles(['vcards/testFile1.vcf', 'vcards/testFile2.vcf'],
+      function (actualResult) {
+        assert.equal(areArraysOfArraysEqual(desiredResult, actualResult), true);
+        done();
+        function areArraysOfArraysEqual (array1, array2) {
+          var result = true;
+          _.map(array1, function (array1Array) {
+            result = _.some(array2, function (array2Array) {
+              return !_.isEqual(array1Array, array2Array);
+            });
+          });
+          return result;
+        }
+      });
   })
 })
